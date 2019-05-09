cb8613be94fd04107fd095acf404c1d35e0eebf3
lutfi <mlutfiibra@outlook.com>
2019-05-09 20:17:16 +0700

    Implement navbar

---
+++ assets/components/HeaderBar.js
@@ -0,0 +1,22 @@
+Vue.component('mynav', {
+    data: function () {
+      return {
+        count: 0
+      }
+    },
+    template: 
+    `
+    <div>
+      <nav class="header">
+        <div class="nav-wrapper">
+          <a href="#" class="brand-logo">My2 Generator</a>
+          <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <li><a href="sass.html">Sass</a></li>
+            <li><a href="badges.html">Components</a></li>
+            <li><a href="collapsible.html">JavaScript</a></li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+    `
+  })
